94c8cfb86c152c4a0fda66ede9220c4e12ff8c63
ahmed omar <ahmedrefay3000@gmail.com>
2024-10-02 22:57:24 +0300

    connect pages in navBar

--- src/components/navbar/TopMenu.tsx
+++ src/components/navbar/TopMenu.tsx
@@ -1,14 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const TopMenu = () => {
     return (
         <div className=" mx-auto w-fit   justify-center items-center sm:py-1 py-0 hidden lg:flex">
             <ul className="flex gap-2 text-sm font-normal ">
                 <li>
-                    <a href="#">تسجيل مستخدم جديد</a>
+                    <Link to="/register">تسجيل مستخدم جديد</Link>
                 </li>
                 <li>
-                    <a href="#">تسجيل الدخول</a>
+                    <Link to="/login">تسجيل الدخول</Link>
                 </li>
                 <li>
                     <a href="#">البحث عن محلاتنا</a>
